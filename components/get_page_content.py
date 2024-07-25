0b0b5ee0f254bec3b1a3b360b52388de83031bf7
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-07-25 16:02:03 -0300

    get_page_content update

--- components/get_page_content.py
+++ components/get_page_content.py
@@ -1,15 +1,8 @@
 import requests
-import os
 
 def get_page_content(url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
-    proxy_url = os.environ.get("http://fixie:W7MlfYNE7TB9k4g@velodrome.usefixie.com:80")
     
-    proxies = {
-        "http": proxy_url,
-        "https": proxy_url
-    }
-    
-    response = requests.get(url, headers=headers, proxies=proxies)
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.content
