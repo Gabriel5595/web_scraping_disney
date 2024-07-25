444589fbe349b6088e34ebcbe30869f55f1ccd5f
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-07-25 15:28:36 -0300

    get_page_content update

--- tests/get_page_content.py
+++ tests/get_page_content.py
@@ -6,7 +6,14 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def get_page_content(url):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36"}
-    response = requests.get(url, headers=headers)
+    proxy_url = os.environ.get("http://fixie:W7MlfYNE7TB9k4g@velodrome.usefixie.com:80")
+    
+    proxies = {
+        "http": proxy_url,
+        "https": proxy_url
+    }
+    
+    response = requests.get(url, headers=headers, proxies=proxies)
     response.raise_for_status()
     return response.content
 
