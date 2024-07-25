70d50084a0a15a2ff57db9bb44a4adaee33f301f
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-07-25 14:25:52 -0300

    get_page_content update

--- components/get_page_content.py
+++ components/get_page_content.py
@@ -2,7 +2,10 @@ import requests
 
 def get_page_content(url):
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive'
     }
     response = requests.get(url, headers=headers)
     response.raise_for_status()
--- tests/get_page_content.py
+++ tests/get_page_content.py
@@ -6,7 +6,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def get_page_content(url):
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive'
     }
     response = requests.get(url, headers=headers)
     response.raise_for_status()
