377f7b9b3cfac631e47bedba799cb0e130179424
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-07-25 14:49:29 -0300

    get_page_content update

--- components/get_page_content.py
+++ components/get_page_content.py
@@ -5,8 +5,11 @@ def get_page_content(url):
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
     'Accept-Language': 'en-US,en;q=0.9',
     'Accept-Encoding': 'gzip, deflate, br',
-    'Connection': 'keep-alive'
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'TE': 'Trailers'
     }
+    
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.content
