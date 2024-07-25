85ebdb0ff66e6a0652f2aedace811d2b4848b847
Gabriel5595 <gabriel.vcarvalho@al.infnet.edu.br>
2024-07-25 14:07:20 -0300

    Header addition to function get_page_content()

--- tests/get_page_content.py
+++ tests/get_page_content.py
@@ -5,7 +5,10 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def get_page_content(url):
-    response = requests.get(url)
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
     response.raise_for_status()
     return response.content
 
